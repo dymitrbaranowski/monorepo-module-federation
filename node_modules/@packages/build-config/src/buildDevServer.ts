899d37e185c89081f39b47ee76c558d67cfd0eae
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-04-20 11:53:49 +0300

    fox1

---
+++ node_modules/@packages/build-config/src/buildDevServer.ts
@@ -0,0 +1,12 @@
+import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
+import { BuildOptions } from "./types/types";
+
+export function buildDevServer(options: BuildOptions): DevServerConfiguration {
+  return {
+    port: options.port ?? 3000,
+    open: true,
+    //Если раздавать статику через nginx или другой сервер, то надо делать проксирование на index.html
+    historyApiFallback: true,
+    hot: true,
+  };
+}
---
+++ node_modules/admin/src/components/App/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link, Outlet } from "react-router-dom";
+
+
+
+export const App = () => {
+
+
+
+
+  return (
+    <div data-testid={"App.DataTestId"}>
+      <h1>PAGE</h1>
+        <Link to={'/about'}>ABOUT</Link>
+        <br/>
+        <Link to={'/shop'}>SHOP</Link>
+        <Outlet/>
+    </div>
+  );
+};
